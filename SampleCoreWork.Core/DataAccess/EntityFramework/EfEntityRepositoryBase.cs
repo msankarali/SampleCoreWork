d60ee87cd97193b1243c8dfb028819e3aed4b243
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-30 02:50:53 +0300

    EF IQueryable implementation added

--- SampleCoreWork.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ SampleCoreWork.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using Microsoft.EntityFrameworkCore;
 using SampleCoreWork.Core.Entities;
 
 namespace SampleCoreWork.Core.DataAccess.EntityFramework
