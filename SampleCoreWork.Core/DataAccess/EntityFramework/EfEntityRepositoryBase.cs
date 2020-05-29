c33c77311a9ffb6bbd2840c6ca41cd7ce9f7d0e4
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-30 02:30:20 +0300

    Entity Framework implementation added

---
+++ SampleCoreWork.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SampleCoreWork.Core.Entities;
+
+namespace SampleCoreWork.Core.DataAccess.EntityFramework
+{
+    class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
