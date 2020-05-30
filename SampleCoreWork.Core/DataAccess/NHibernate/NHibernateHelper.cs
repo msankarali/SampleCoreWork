e937942d8f416709631673e90819f4440ddd3423
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-30 03:12:02 +0300

    NHibernate helper added

---
+++ SampleCoreWork.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NHibernate;
+
+namespace SampleCoreWork.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public virtual ISessionFactory SessionFactory => _sessionFactory ?? (_sessionFactory = InitializeFactory());
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
