c578a91294bca8d3396ed5e456668f7f5e52e936
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-30 03:29:56 +0300

    NHibernate IQueryable implementation added

---
+++ SampleCoreWork.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SampleCoreWork.Core.Entities;
+
+namespace SampleCoreWork.Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        public virtual IQueryable<T> Entities => _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>());
+    }
+}
