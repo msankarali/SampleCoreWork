4fc562640314e31fa6773dbc60a631cf7fc212fd
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-30 03:24:29 +0300

    NHibernate entity repository base added

---
+++ SampleCoreWork.Core/DataAccess/NHibernate/NhEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using SampleCoreWork.Core.Entities;
+using NHibernate.Linq;
+
+namespace SampleCoreWork.Core.DataAccess.NHibernate
+{
+    public class NhEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+
+        public NhEntityRepositoryBase(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Save(entity);
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Delete(entity);
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                return session.Query<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                return filter == null
+                    ? session.Query<TEntity>().ToList()
+                    : session.Query<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                session.Update(entity);
+                return entity;
+            }
+        }
+    }
+}
