868efb99d50dc768accc34bb47719c701f72c0a5
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-04 01:49:36 +0300

    NHibernate SqlServerHelper implamentation

---
+++ SampleCoreWork.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using SampleCoreWork.Core.DataAccess.NHibernate;
+
+namespace SampleCoreWork.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHelper : NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently
+                .Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2012
+                        .ConnectionString(c => c.Database("NorthwindContext")
+                        )
+                )
+                .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .BuildSessionFactory();
+        }
+    }
+}
