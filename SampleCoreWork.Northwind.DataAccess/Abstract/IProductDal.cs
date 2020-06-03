f2bc15e17655844d2b70e0fd9e56807452913a48
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-04 00:33:52 +0300

    IEntityRepository implementation on DataAccess Layer

---
+++ SampleCoreWork.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SampleCoreWork.Core.DataAccess;
+using SampleCoreWork.Northwind.Entities.Concrete;
+
+namespace SampleCoreWork.Northwind.DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+    }
+}
---
+++ SampleCoreWork.Northwind.Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SampleCoreWork.Core.Entities;
+
+namespace SampleCoreWork.Northwind.Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
