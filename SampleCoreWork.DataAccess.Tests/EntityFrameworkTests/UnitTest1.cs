553b11b57e46ecbb137ce1ba6fd987736d880784
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-04 01:19:09 +0300

    Tests for DataAccess

---
+++ SampleCoreWork.DataAccess.Tests/EntityFrameworkTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleCoreWork.Northwind.DataAccess.Concrete.EntityFramework;
+
+namespace SampleCoreWork.DataAccess.Tests.EntityFrameworkTests
+{
+    [TestClass]
+    public class EntityFrameworkTest
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+
+            var result = productDal.GetList();
+
+            Assert.AreEqual(77, result.Count);
+        }
+
+        [TestMethod]
+        public void Get_all_with_parameter_returns_filtered_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+
+            var result = productDal.GetList(p => p.ProductName.Contains("no"));
+
+            Assert.AreEqual(6, result.Count);
+        }
+    }
+}
