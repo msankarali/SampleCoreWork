4d6be94fdf901a5d59b82bd68f4cc6463f1306df
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-04 01:55:52 +0300

    NHibernate Mapping

---
+++ SampleCoreWork.Northwind.DataAccess/Concrete/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentNHibernate.Mapping;
+using SampleCoreWork.Northwind.Entities.Concrete;
+
+namespace SampleCoreWork.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table(@"Products");
+            LazyLoad();
+            Id(x => x.ProductId).Column("ProductID");
+            Map(x => x.CategoryId).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+        }
+    }
+}
