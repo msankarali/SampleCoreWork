3318e1abca7a22f9f4a66bbc4054422a0dc66505
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-18 19:43:09 +0300

    Client Side Validation DI

---
+++ SampleCoreWork.Northwind.Business/DependencyResolver/Ninject/ValidationModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Ninject.Modules;
+using SampleCoreWork.Northwind.Business.ValidationRules.FluentValidation;
+using SampleCoreWork.Northwind.Entities.Concrete;
+
+namespace SampleCoreWork.Northwind.Business.DependencyResolver.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidator>().InSingletonScope();
+        }
+    }
+}
