911c13fa53a41b8abb46f8f62c98e07021df3d44
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-06-18 19:27:36 +0300

    DI

---
+++ SampleCoreWork.Northwind.Business/DependencyResolver/Ninject/BusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ninject.Modules;
+using SampleCoreWork.Northwind.Business.Abstract;
+using SampleCoreWork.Northwind.Business.Concrete.Managers;
+using SampleCoreWork.Northwind.DataAccess.Abstract;
+using SampleCoreWork.Northwind.DataAccess.Concrete.EntityFramework;
+
+namespace SampleCoreWork.Northwind.Business.DependencyResolver.Ninject
+{
+    public class BusinessModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>();
+        }
+    }
+}
